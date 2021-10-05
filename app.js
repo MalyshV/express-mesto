a84739fa801e0597f1bddf04aaf4dc8e4c007f6f
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-10-06 00:22:12 +0300

    feat: connect mongoDB

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const PORT = 3000;
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
 
 app.listen(PORT, () => {
   console.log("Express is running");
