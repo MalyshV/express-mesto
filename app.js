d02ba72eb444e465d7d852d6ea35b43a3ac07a4c
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-10-12 16:19:46 +0300

    fix: add orFail() for correct work of controlles & fix mistakes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+const ERROR_CODE_404 = 404;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -21,6 +24,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
+app.use(helmet());
+
+app.all('*', (req, res) => {
+  res.status(ERROR_CODE_404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listen ${PORT}`);
--- controllers/cards.js
+++ controllers/cards.js
@@ -3,10 +3,11 @@ const Card = require('../models/card');
 const ERROR_CODE_500 = 500;
 const ERROR_CODE_400 = 400;
 const ERROR_CODE_404 = 404;
+const OK_CODE_200 = 200;
 
 const getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.status(OK_CODE_200).send({ data: cards }))
     .catch(() => {
       res.status(ERROR_CODE_500).send({ message: 'Ошибка сервера' });
     });
@@ -16,22 +17,31 @@ const postCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.status(OK_CODE_200).send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(ERROR_CODE_400).send({ message: 'Переданы некорректные данные при создании карточки' });
+        return res.status(ERROR_CODE_404).send({ message: 'Переданы некорректные данные при создании карточки' });
       }
       return res.status(ERROR_CODE_500).send({ message: 'Ошибка сервера' });
     });
 };
 
 const deleteCard = (req, res) => {
-  const { userId } = req.params;
+  const { cardId } = req.params;
 
-  Card.findByIdAndRemove(userId)
-    .then((card) => res.send({ data: card }))
-    .catch(() => {
-      res.status(ERROR_CODE_404).send({ message: 'Карточка с указанным _id не найдена' });
+  Card.findByIdAndRemove(cardId)
+    .then((card) => {
+      if (card) {
+        res.status(OK_CODE_200).send({ data: card });
+      } else {
+        res.status(ERROR_CODE_404).send({ message: 'Карточка с указанным _id не найдена' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE_400).send({ message: 'Переданы некорректные данные при удалении карточки' });
+      }
+      return res.status(ERROR_CODE_500).send({ message: 'Ошибка сервера' });
     });
 };
 
@@ -41,10 +51,16 @@ const likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
+    .orFail(new Error('NotValidId'))
+    .then((card) => {
+      res.status(OK_CODE_200).send({ data: card });
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
         return res.status(ERROR_CODE_400).send({ message: 'Переданы некорректные данные для постановки лайка' });
+      }
+      if (err.message === 'NotValidId') {
+        return res.status(ERROR_CODE_404).send({ message: 'Передан несуществующий _id карточки' });
       }
       return res.status(ERROR_CODE_500).send({ message: 'Ошибка сервера' });
     });
@@ -56,10 +72,16 @@ const dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
+    .orFail(new Error('NotValidId'))
+    .then((card) => {
+      res.status(OK_CODE_200).send({ data: card });
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(ERROR_CODE_400).send({ message: 'Переданы некорректные данные для снятии лайка' });
+        return res.status(ERROR_CODE_400).send({ message: 'Переданы некорректные данные для снятия лайка' });
+      }
+      if (err.message === 'NotValidId') {
+        return res.status(ERROR_CODE_404).send({ message: 'Передан несуществующий _id карточки' });
       }
       return res.status(ERROR_CODE_500).send({ message: 'Ошибка сервера' });
     });
